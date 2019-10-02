a04badaed4ef221f884faee41a919a38df655643
unknown <frankdavid93om@yahoo.com>
2019-10-02 13:29:13 -0400

    adding a navbar component example

---
+++ src/components/navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function NavBar() {
+  return (
+    <nav>
+        <h1>Example of a Nav Component to use it in all of the templates</h1>
+    </nav>
+  );
+}
+
+export default NavBar;
